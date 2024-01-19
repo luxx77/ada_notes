b536896c692d853ddc64257df116252986967544
luxx77 <luxxicm@gmail.com>
2024-01-19 15:37:05 +0500

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'my_app.dart';
+
 // its my first app yehuu
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp], )
-      .then((value) => runApp(const MyApp()));
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  ).then((value) => runApp(const MyApp()));
 }
